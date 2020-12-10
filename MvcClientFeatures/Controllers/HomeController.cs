8aa3add6bede954fdc7c280b8fc25e522dafb101
kasia.kordys <kasia.kordys@gmail.com>
2020-12-10 19:04:24 +0000

    add home controller

---
+++ MvcClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using MvcClientFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult MakeBooking()
+        {
+            return View(new Appointment
+            { ClientName = "Adam",
+            TermsAccepted = true    
+           });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appo)
+        {
+            return Json(appo, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
